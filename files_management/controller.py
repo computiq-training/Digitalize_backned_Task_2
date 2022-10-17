d7310f5ef36400e00b4d27ffa3287468cbedef44
noor <noorsaadjabar@gmail.com>
2022-10-17 23:58:45 +0300

    task 2

--- files_management/controller.py
+++ files_management/controller.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
 # Create your views here.
+from ninja import Router
+
+post_controller = Router()
+
+@post_controller.get('/')
+def list_post(request):
+    return{'Message:goooo'}
