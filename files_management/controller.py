39e7934c8d9165206dbc0b08edc0e34228594bd6
unknown <zahraa.cs531@student.uomosul.edu.iq>
2022-10-22 01:46:04 +0300

    solved the task2

--- files_management/controller.py
+++ files_management/controller.py
@@ -1,2 +1,45 @@
 from django.shortcuts import render
 # Create your views here.
+
+# Create your views here.
+import re
+from ninja import Router
+posts_controller=Router()
+
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+
+
+@posts_controller.get('')
+def list_posts(request):
+    """
+    Returns a list of all names of blog posts.
+    """
+    _, filenames = default_storage.listdir("posts")
+    return list(sorted(re.sub(r"\.md$", "", filename)
+                       for filename in filenames if filename.endswith(".md")))
+
+@posts_controller.post('/{title}')
+
+def save_post(request,title, content):
+    """
+    Saves a blog post, given its title and Markdown
+    content. If an existing post with the same title already exists,
+    it is replaced.
+    """
+    filename = f"posts/{title}.md"
+    if default_storage.exists(filename):
+        default_storage.delete(filename)
+    default_storage.save(filename, ContentFile(content))
+
+@posts_controller.get('/{title}')
+def get_post(request,title ):
+    """
+    Retrieves a post by its title. If no such
+    post exists, the function returns None.
+    """
+    try:
+        f = default_storage.open(f"posts/{title}.md")
+        return f.read().decode("utf-8")
+    except FileNotFoundError:
+        return None
