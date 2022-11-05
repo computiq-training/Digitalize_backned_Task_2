3e5b8866bb3ca65932bf6df29f0a1bb35e0243a3
hiba707 <105562151+hiba707@users.noreply.github.com>
2022-11-05 10:06:05 +0300

    Update controller.py

--- files_management/controller.py
+++ files_management/controller.py
@@ -1,2 +1,25 @@
 from django.shortcuts import render
-# Create your views here.
+from ninja import Router 
+import re
+from files_management.utils import list_posts,save_post,get_post,del_post
+
+post_controller = Router()
+@post_controller.get('')
+def list_posts(request):
+  return list_posts()
+
+
+@post_controller.get('/{title}')
+def get_post(request ,title):
+  return get_post(title)
+
+@post_controller.post('')
+def save_post(request , title , content):
+  return save_post(title,content)
+
+
+
+@post_controller.delete('/{title}')
+def del_post(request ,title):
+  return del_post(title)
+
