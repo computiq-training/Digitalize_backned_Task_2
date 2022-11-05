fec6660c086e14f5f7c4d55c2fc46984f5d8bf04
Redab Othman <104794234+redabh-othman@users.noreply.github.com>
2022-11-05 15:39:47 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
+from files_management.controller import post_controller
 
+api=NinjaAPI()
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/' , post_controller)
+
 ]
+
